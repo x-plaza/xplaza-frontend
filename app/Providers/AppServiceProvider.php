1b8f79adadd23918b9823b5ae2a3e62868218f2a
Mahiuddin Al Kamal <mahiuddinalkamal@gmail.com>
2021-12-29 04:30:31 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app->bind('path.public', function() {
-        return base_path().'/../public_html';
-        });
+        //
     }
 
     /**
