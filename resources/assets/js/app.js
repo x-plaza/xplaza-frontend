0dbf3b0e1c38d66db79e46a1995e9d17b71b9a39
Mahiuddin Al Kamal <mahiuddinalkamal@gmail.com>
2025-05-29 08:54:55 +0200

    fixed vue3 issue, added docker build in pr pipeline

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,24 +1,6 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import { createApp } from "vue";
+import ExampleComponent from "./components/ExampleComponent.vue";
 
-require("./bootstrap");
-
-window.Vue = require("vue");
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component(
-  "example-component",
-  require("./components/ExampleComponent.vue"),
-);
-
-const app = new Vue({
-  el: "#app",
-});
+const app = createApp({});
+app.component("example-component", ExampleComponent);
+app.mount("#app");
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,11 @@
 let mix = require("laravel-mix");
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix
   .js("resources/assets/js/app.js", "public/js")
+  .vue()
   .sass("resources/assets/sass/app.scss", "public/css");
+
+// Optional: Add versioning for production builds
+if (mix.inProduction()) {
+  mix.version();
+}
